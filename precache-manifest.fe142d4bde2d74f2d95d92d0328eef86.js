10dbd0bf6046b24738802302f4b03fbe7289becb
Peter Kiss <ypetya@gmail.com>
2019-01-13 15:27:07 +0100

    sec

---
+++ precache-manifest.fe142d4bde2d74f2d95d92d0328eef86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f64a83354cf8e01c18e3",
+    "url": "https://ypetya.github.io/map-beacon/static/js/runtime~main.f64a8335.js"
+  },
+  {
+    "revision": "ab3b49972f30ea253e99",
+    "url": "https://ypetya.github.io/map-beacon/static/js/main.ab3b4997.chunk.js"
+  },
+  {
+    "revision": "b0fcd41e240e2fde75e5",
+    "url": "https://ypetya.github.io/map-beacon/static/js/1.b0fcd41e.chunk.js"
+  },
+  {
+    "revision": "ab3b49972f30ea253e99",
+    "url": "https://ypetya.github.io/map-beacon/static/css/main.27aed5a1.chunk.css"
+  },
+  {
+    "revision": "42101625b530039686efde39a7e514be",
+    "url": "https://ypetya.github.io/map-beacon/index.html"
+  }
+];
